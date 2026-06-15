db1056f645e3d4afff555ca03f7967d7f6c4a6f7
Syed Sayeel Abbas <syedsayeelabbas@gmail.com>
2026-06-15 18:56:48 +0500

    Add files via upload

---
+++ lecture 2/minproject1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "===== STUDENT PROFILE =====\n\n";
+
+    cout << "Name: Syed Sayeel Abbas\n";
+    cout << "Age: 18\n";
+    cout << "University: ABC University\n";
+    cout << "Department: Software Engineering\n";
+    cout << "Favorite Language: C++\n\n";
+
+    cout << "===========================";
+
+    return 0;
+}
