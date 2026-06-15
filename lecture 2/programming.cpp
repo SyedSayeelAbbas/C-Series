2ec5728b9b06070a2dd86a2e1868d11b24317062
Syed Sayeel Abbas <syedsayeelabbas@gmail.com>
2026-06-15 18:56:23 +0500

    Create programming.cpp

---
+++ lecture 2/programming.cpp
@@ -0,0 +1,9 @@
+// Computer(Machine) => instruction => machine language
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"Welcome to C++ Programming\n";
+  cout<<"Welcome to C++ Programming\n";
+  cout<<"Welcome to C++ Programming";
+  return 0;
+}
