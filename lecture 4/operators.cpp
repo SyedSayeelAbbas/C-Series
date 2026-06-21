f3962f5c5136af216e1049f96131880de372ac2a
Syed Sayeel Abbas <syedsayeelabbas@gmail.com>
2026-06-21 14:19:43 +0500

    Add operators.cpp with basic operator examples

---
+++ lecture 4/operators.cpp
@@ -0,0 +1,60 @@
+// - + * / %
+#include<iostream>
+using namespace std;
+int main(){
+  // cout<<5+5<<endl;
+  // cout<<5<<5<<"5";
+
+  // int number1 = 2;
+  // int number2 = 3;
+
+  // cout<<number1 + number2<<endl;
+  // cout<<number1 - number2<<endl;
+  // cout<<number1 * number2<<endl;
+  // cout<<number1 / number2<<endl;
+  // cout<<number1% number2<<endl;
+
+  // Relational
+  // == , != , < , > , <= , >=
+
+  // cout<< (2==2)<<endl;
+  // cout<< (3!=2)<<endl;
+  // cout<< (3<2)<<endl;
+  // cout<< (3>2)<<endl;
+  // cout<< (3<=3)<< endl;
+  // cout<< (3>=3)<< endl;
+
+  //Assignment Operators
+  // int num = 2;
+  // num = num *2;
+  // num+=2;
+  // cout<<num<<endl;//4
+  // num-=2;
+  // cout<<num<<endl;//2
+  // num*=2;
+  // cout<<num<<endl;//4
+  // num/=2;
+  // cout<<num<<endl;//2
+
+  //Logical Operators
+  // || , && , !
+  // int num1 = 2;
+  // int num2 = 3;
+  // cout<<(num1==num2 && num1!=num2);//0
+  // cout<<(num1==num2 || num1!=num2);//1
+  // cout<<(!(num1==num2));
+
+  //Increament Decreament
+  //++ , --
+  // int num = 2;
+  // cout<<num++<<endl;
+  // cout<<num--<<endl;
+  // cout<<num;
+
+  //BODMAS B=>() , D=> / , M=> * , A=> + , S=> -
+  
+  int value = (2+2)*3;
+  cout<<value;
+
+  return 0;
+}
