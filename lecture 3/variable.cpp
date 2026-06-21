7e9e793a775719c5687bc5e5e803e7b0c210a1cc
Syed Sayeel Abbas <syedsayeelabbas@gmail.com>
2026-06-21 14:18:34 +0500

    Add variable.cpp with variable examples and comments

---
+++ lecture 3/variable.cpp
@@ -0,0 +1,91 @@
+//variables
+#include<iostream>
+using namespace std;
+int main(){
+
+ //What is Variables?
+//  string p1 = "Sayeel";
+//  string p2 = "Saim";
+//  string p3 = "ali";
+//  string p4 = "Sayal";
+//  string email = "iuahfiuhwiuhtr834798taurngfoaweoiuq308ut0q3tgijerwqiotj90";
+// //  cout<<p1+"\n";
+// //  cout<<p2+"\n";
+// //  cout<<p3+"\n";
+// //  cout<<p4+"\n";
+// //  cout<<email;
+
+//  // declarization
+//  int something;
+//  // Assingment
+//  something = 90;
+//  //instinlization
+//  float percent = 30.8;
+
+
+//   // const use
+//   //Before
+//   int income = 10000;
+  // cout<<income;
+  // cout<<"\n";
+  // income = 900000;
+  // cout<< income;
+  //After
+  // const int income2=45;
+  // cout<<income2;
+  // income2 = 55;
+  // cout<< income2;
+
+
+  //1) canot use same variable name again for another variable
+  // int income;
+  // float income;
+  //2) use data type first then variable name ( datatype variableName = Data;  )
+  // how you should use
+  // string  age;
+  // string studentName;
+  // float totalMarks;
+  // int _salary;
+
+  // cout<<age; // cout<<Age
+  // // how you should not use
+  // int;
+  // int 2name; // int name2, int n2mae, / " . " >< }
+  // int person Name; // person_Name
+
+
+  //int personNameOnGame Cammel cassing
+ // type
+//  int number = 2;
+//  float decimal = 4.9;
+//  string sentenc = "eat food";
+//  bool isMale = false;
+//  char character = '.';
+ // naming convention
+ // opterator
+ // espace sequnce
+
+
+//  int age = 18;
+//     float height = 6.3;
+//     const double pi = 3.1415926535;
+//     char grade = 'A';
+//     bool isStudent = true;
+//     string name = "Sayeel";
+
+//     cout << name << endl;
+//     cout << age << endl;
+//     cout << height << endl;
+//     cout << pi << endl;
+//     cout << grade << endl;
+//     cout << isStudent << endl;
+
+//  int age;
+//  cout<<"Enter your Age :"<<endl;
+//  cin>>age;
+//  cout<<"Your Age is :"<<age;
+
+cout<<"\'Sayeel\'";
+
+ return 0;
+}
