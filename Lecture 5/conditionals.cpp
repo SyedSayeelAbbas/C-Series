1a25895b0d25411a55aa0025061b0ef84ad10d68
Syed Sayeel Abbas <syedsayeelabbas@gmail.com>
2026-06-23 14:55:24 +0500

    Create conditionals.cpp

---
+++ Lecture 5/conditionals.cpp
@@ -0,0 +1,160 @@
+// CONDIONTIONALS
+// if , if else , else , switch , break , defualt , age calcutor , grading calculator , ATM
+#include<iostream>
+using namespace std;
+int main(){
+  // int age;
+
+  // cout<<"Enter your age :"<<endl;
+  // cin>>age;
+  
+  // if (age>=18) {
+  //   cout<<"Sayeel"<<endl;
+  //   cout<<age;
+  // } else if (age<18 && age >=0) {
+  //   cout<<age;
+  // } else{
+  //   cout<<"enter valid value";
+  // }
+
+  // float percentage;
+  // int mark1,mark2,mark3;
+
+  // cout<<"Enter you Urdu marks:";
+  // cin>>mark1;
+  // cout<<endl;
+  // cout<<"Enter you English marks:";
+  // cin>>mark2;
+  // cout<<endl;
+  // cout<<"Enter you Maths marks:";
+  // cin>>mark3;
+  // cout<<endl;
+
+  // percentage = ((mark1+mark2+mark3)/300.0)*100;
+
+  // if( (mark1>=0 && mark1<=100) && (mark1>=0 && mark1<=100) && (mark1>=0 && mark1<=100)){
+  //   if(percentage>=90 && percentage<=100){
+  //     cout<<percentage<<endl;
+  //     cout<<"A+";
+  //   } else if (percentage<=89 && percentage>=80)
+  //   {
+  //     cout<<percentage<<endl;
+  //     cout<<"A";
+  //   } else if (percentage <= 79 && percentage >= 70)
+  //   {
+  //     cout<<percentage<<endl;
+  //     cout<<"B";
+  //   } else {
+  //     cout<<percentage<<endl;
+  //     cout<<"F";
+  //   }    
+  // } else{
+  //   cout<<"enter valid range which is (0-100)";
+  // }
+
+
+  // int day = 1 ;
+
+  // switch (day)
+  // {
+  // case 1:
+  //   cout<<"monday";
+  //   break;
+  // case 2:
+  //   cout<<"tuesday";
+  //   break;
+  // case 3:
+  //   cout<<"wednesday";
+  //   break;
+  // case 4:
+  //   cout<<"thursday";
+  //   break;
+  // case 5:
+  //   cout<<"friday";
+  //   break;
+  // case 6:
+  //   cout<<"saturday";
+  //   break;
+  // case 7:
+  //   {cout<<"sunday"<<endl;
+  //   cout<<"done";
+  //   }
+  //   break;
+  
+  // default:
+  //   cout<<"enter valid value";
+  //   break;
+  // }
+
+
+  // if((mark1>=0 && mark1<=100) && (mark2>=0 && mark2<=100) && (mark3>=0 && mark3<=100)){
+    
+  //   cout << "Percentage: " << percentage << "%" << endl;
+
+  //   int gradeKey = (int)percentage / 10;
+
+  //   switch(gradeKey) {
+  //     case 10: // 100%
+  //     case 9:  // 90% - 99%
+  //       cout << "Grade: A+" << endl;
+  //       break;
+  //     case 8:  // 80% - 89%
+  //       cout << "Grade: A" << endl;
+  //       break;
+  //     case 7:  // 70% - 79%
+  //       cout << "Grade: B" << endl;
+  //       break;
+  //     default: // Anything below 70%
+  //       cout << "Grade: F" << endl;
+  //       break;
+  //   }
+    
+  // } else {
+  //   cout<<"enter valid range which is (0-100)";
+  // }
+
+
+  // int balace = 100,choice,deposit;
+
+
+  // cout<<"1)check balance\t\t2)deposit\n\n3)withdraw\t\t4)exit\n\n";
+  // cout<<"ENter your coice:";
+  // cin>>choice;
+  // cout<<endl;
+
+  // switch (choice)
+  // {
+  // case 1:
+  //   cout<<"balance:"<<balace;
+  //   break;
+
+  // case 2:
+  // {
+  //   cout<<"Enter your Amonut:";
+  //   cin>>deposit;
+  //   balace+=deposit;
+  //   cout<<"balance:"<<(balace);
+  // }
+  // break;
+
+  // case 3:
+  // {
+  //   cout<<"Enter your Amonut:";
+  //   cin>>deposit;
+  //   balace-=deposit;
+  //   cout<<"balance:"<<(balace);
+  // }
+  // break;
+  // case 4:
+  //   cout<<"bye bye";
+  //   break;
+  
+  // default:
+  //   cout<<"error";
+  //   break;
+  // }
+
+
+
+  return 0;
+}
