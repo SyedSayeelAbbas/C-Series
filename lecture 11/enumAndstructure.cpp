79a12cab3eeb3d08c87b27efeeb996dc37ae6929
Syed Sayeel Abbas <syedsayeelabbas@gmail.com>
2026-06-27 20:53:38 +0500

    Create enumAndstructure.cpp

---
+++ lecture 11/enumAndstructure.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+// struct Address
+// {
+//   int postcode;
+//   string country;
+// };
+
+// struct Person{
+//   string name;
+//   int age;
+//   Address add;
+// };
+
+// enum day {
+//   monday,
+//   tuesday,
+//   sunday
+// };
+
+int main(){
+  // day abhi = sunday;
+  // switch (abhi)
+  // {
+  // case sunday:
+  //   cout<<"t";
+  //   break;
+  
+  // default:
+  //   break;
+  }
+  // Person p[2];
+
+  // for(int i =0;i<2;i++){
+  //   cout<<"Enter name :"<<endl;
+  //   cin>>p[i].name;
+  //   cout<<"Enter age :"<<endl;
+  //   cin>>p[i].age;
+  //   cout<<"Enter postal code :"<<endl;
+  //   cin>>p[i].add.postcode;
+  //   cout<<"Enter Country :"<<endl;
+  //   cin>>p[i].add.country;
+  // }
+
+  // for(int i =0;i<2;i++){
+  //   cout<<"==========Person NO :"<<i<<"=============";
+  //   cout<<p[i].name<<endl;
+  //   cout<<p[i].age<<endl;
+  //   cout<<p[i].add.postcode<<endl;
+  //   cout<<p[i].add.country<<endl;
+  // }
+
+  // p1.name = "sayeel";
+  // p1.age = 19;
+  // p1.add.postcode =90;
+  // p1.add.country ="Pakistan";
+
+  // p2.name = "saim";
+  // p2.age = 18;
+  // p2.add.postcode =190;
+  // p2.add.country ="USA";
+  
+  // cout<<p1.name<<endl;
+  // cout<<p1.age<<endl;
+  // cout<<p1.add.postcode<<endl;
+  // cout<<p1.add.country<<endl;
+
+  // cout<<p2.name<<endl;
+  // cout<<p2.age<<endl;
+  // cout<<p2.add.postcode<<endl;
+  // cout<<p2.add.country<<endl;
+  return 0;
+}
