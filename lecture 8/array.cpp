cd85e7c11ae3b596a4e989417081cd1e9e08c081
Syed Sayeel Abbas <syedsayeelabbas@gmail.com>
2026-06-27 20:32:15 +0500

    Add examples of array usage in C++
    This file contains various examples of array usage in C++, including single and multi-dimensional arrays, input/output operations, and iteration methods.

---
+++ lecture 8/array.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<array>
+using namespace std;
+int main(){
+  //int var1=56,var2,var3=2;
+  // datatype variableName[space] = {value,value}
+  // int setOfWhole[5] = {1,3,5,4,7}; //1=0,3=1,5=2,5=3
+  // cout<<setOfWhole[4]<<endl;
+  // setOfWhole[2]=4;
+  // cout<<setOfWhole[2]<<endl;
+
+  // //Decliration
+  // int arr[3];
+  // //Assingment
+  // // arr[0]=1;
+  // // arr[1]=5;
+  // // arr[2]=10;
+  // // cout<<arr[0]<<endl;
+  // // cout<<arr[1]<<endl;
+  // // cout<<arr[2]<<endl;
+  // for(int i=0;i<3;i++){
+  //   cout<<"Enter value for Array on Index no "<<i<<":";
+  //   cin>>arr[i];
+  // }
+  // for(int i=0;i<3;i++){
+  //   cout<<arr[i]<<endl;
+  // }
+  // array<int, 5> arr = {10, 20, 30, 40, 50};
+
+  // for(int i =0;i<arr.size();i++){
+  //   if(arr[i]==10){
+  //     cout<<"We Found the Value "<<arr[i];
+  //     break;
+  //   }
+  //   cout<<"Current value is "<<arr[i]<<" on "<<(i+1)<<" iteration"<<endl;
+  // }
+
+  // int arr[3]={3,4,5};
+
+  // for(const auto& a:arr){
+  //   cin>>a;
+  // }
+  // for(const & a:arr){
+  //   cout<<a<<endl;
+  // }
+
+  // int arr[4]={10,20,30,40};
+
+  // for(int i = 2;i<3;i++){
+  //   arr[i]=arr[i+1];
+  // }
+  // for(const auto& a:arr){
+  //   cout<<a<<endl;
+  // }
+
+  // int arr[2][3] = {
+  //   {2,4,6},
+  //   {1,8,9}
+  // };
+  // cout<<arr[1][2];
+
+  // for(int i =0;i<2;i++){
+  //   for(int j=0;j<3;j++){
+  //     cout<<arr[i][j];
+  //   }
+  //   cout<<endl;
+    
+  // }
+  
+
+  return 0;
+}
